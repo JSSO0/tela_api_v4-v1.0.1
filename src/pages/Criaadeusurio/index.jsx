f0202d6a7e24dfbac88b35f7ef41fd43237d40bb
Joely <sjoely131@gmail.com>
2023-12-28 15:02:48 -0300

    funcionando

--- src/pages/Criaadeusurio/index.jsx
+++ src/pages/Criaadeusurio/index.jsx
@@ -1,8 +1,62 @@
-import React from "react";
-
+import React, { useState } from "react";
 import { Button, Img, Input, Text } from "components";
+import { createUser } from "service/userservice";
 
 const CriaadeusurioPage = () => {
+
+
+  const [userData, setUserData] = useState({
+    nome: "",
+    telefone: "",
+    email: "",
+    cpf: "",
+    username: "",
+    password: "",
+  });
+
+  const [result, setResult] = useState(null);
+
+  const handleInputChange = (fieldName, value) => {
+    setUserData((prevUserData) => ({
+      ...prevUserData,
+      [fieldName]: value,
+    }));
+  };
+
+  const handleCreateUser = async () => {
+    try {
+      // Chame a função do serviço passando os dados do usuário
+      const response = await createUser(userData);
+
+      // Atualize o estado com a resposta da chamada da API
+      setResult(response);
+
+      // Limpe o estado após o sucesso, se necessário
+      setUserData({
+        nome: "",
+        telefone: "",
+        email: "",
+        cpf: "",
+        username: "",
+        password: "",
+      });
+
+      // Retorne a resposta da chamada da API
+      return response;
+    } catch (error) {
+      // Trate erros, se necessário
+      console.error("Erro ao criar usuário:", error);
+
+      // Limpe o resultado em caso de erro
+      setResult(null);
+
+      // Retorne null ou lance o erro para ser tratado pelo chamador
+      return null;
+    }
+  };
+
+
+
   return (
     <>
       <div className="bg-blue-100 font-inter h-[832px] mx-auto p-0.5 relative w-full">
@@ -26,16 +80,25 @@ const CriaadeusurioPage = () => {
               Preencha os dados abaixo!
             </Text>
           </div>
+
           <div className="absolute bottom-[9%] flex flex-col gap-2 inset-x-[0] items-center justify-start mx-auto w-[19%]">
-            <Button className="cursor-pointer font-extrabold leading-[normal] min-w-[217px] text-center text-xs">
-              Criar usuário
-            </Button>
+
+             <Button
+        className="cursor-pointer font-extrabold leading-[normal] min-w-[217px] text-center text-xs"
+        onClick={handleCreateUser}
+      >
+        Criar usuário
+      </Button>
+
             <div className="flex flex-col items-center justify-start w-full">
-              <Button className="cursor-pointer font-extrabold leading-[normal] min-w-[217px] text-center text-xs" onClick={() => navigate("/")}>
+              <Button className="cursor-pointer font-extrabold leading-[normal] min-w-[217px] text-center text-xs" >
                 Voltar
               </Button>
             </div>
+
           </div>
+
+
           <div className="absolute flex flex-col items-start justify-start left-[0] rounded-[10px] shadow-bs top-[27%] w-[44%]">
             <Text className="text-black-900 text-xs" size="txtInterExtraBold12">
               Nome
@@ -44,9 +107,11 @@ const CriaadeusurioPage = () => {
               name="rectangleFive"
               placeholder=""
               className="p-0 w-full"
-              wrapClassName="border border-black-900 border-solid flex h-[39px] mt-1 w-full"
+              wrapClassName="border border-black-900 border-solid flex h-[39px] mt-1 w-full"    onChange={(e) => handleInputChange("nome", e)}  // Passando o evento completo
+              value={userData.nome}
             ></Input>
           </div>
+
           <div className="absolute flex flex-col items-start justify-start left-[0] rounded-[10px] shadow-bs top-[35%] w-[44%]">
             <Text className="text-black-900 text-xs" size="txtInterExtraBold12">
               Usuário
@@ -55,9 +120,11 @@ const CriaadeusurioPage = () => {
               name="rectangleSix"
               placeholder=""
               className="p-0 w-full"
-              wrapClassName="border border-black-900 border-solid flex h-[39px] mt-[3px] w-full"
+              wrapClassName="border border-black-900 border-solid flex h-[39px] mt-[3px] w-full"   onChange={(e) => handleInputChange("username", e)}  // Passando o evento completo
+              value={userData.username}
             ></Input>
           </div>
+
           <div className="absolute flex flex-col items-start justify-start left-[0] rounded-[10px] shadow-bs top-[42%] w-[44%]">
             <Text className="text-black-900 text-xs" size="txtInterExtraBold12">
               CPF
@@ -66,9 +133,11 @@ const CriaadeusurioPage = () => {
               name="rectangleSeven"
               placeholder=""
               className="p-0 w-full"
-              wrapClassName="border border-black-900 border-solid flex h-[39px] mt-0.5 w-full"
+              wrapClassName="border border-black-900 border-solid flex h-[39px] mt-0.5 w-full"    onChange={(e) => handleInputChange("cpf", e)}  // Passando o evento completo
+              value={userData.cpf}
             ></Input>
           </div>
+
           <div className="absolute bottom-[44%] flex flex-col items-start justify-start left-[0] rounded-[10px] shadow-bs w-[44%]">
             <Text className="text-black-900 text-xs" size="txtInterExtraBold12">
               Senha
@@ -77,9 +146,11 @@ const CriaadeusurioPage = () => {
               name="rectangleEight"
               placeholder=""
               className="p-0 w-full"
-              wrapClassName="border border-black-900 border-solid flex h-[39px] mt-0.5 w-full"
+              wrapClassName="border border-black-900 border-solid flex h-[39px] mt-0.5 w-full"    onChange={(e) => handleInputChange("password", e)}  // Passando o evento completo
+              value={userData.password}
             ></Input>
           </div>
+
           <div className="absolute bottom-[36%] flex flex-col gap-[5px] items-start justify-start left-[0] rounded-[10px] shadow-bs w-[44%]">
             <Text className="text-black-900 text-xs" size="txtInterExtraBold12">
               Telefone
@@ -88,20 +159,24 @@ const CriaadeusurioPage = () => {
               name="rectangleNine"
               placeholder=""
               className="p-0 w-full"
-              wrapClassName="border border-black-900 border-solid flex h-[39px] w-full"
+              wrapClassName="border border-black-900 border-solid flex h-[39px] w-full"    onChange={(e) => handleInputChange("telefone", e)}  // Passando o evento completo
+              value={userData.telefone}
             ></Input>
           </div>
+
           <div className="absolute bottom-[28%] flex flex-col gap-2 items-start justify-start left-[0] rounded-[10px] shadow-bs w-[44%]">
             <Text className="text-black-900 text-xs" size="txtInterExtraBold12">
-              Número
+              E-mail
             </Text>
             <Input
               name="rectangleTen"
               placeholder=""
               className="p-0 w-full"
-              wrapClassName="border border-black-900 border-solid flex h-[39px] w-full"
+              wrapClassName="border border-black-900 border-solid flex h-[39px] w-full"    onChange={(e) => handleInputChange("email", e)}  // Passando o evento completo
+              value={userData.email}
             ></Input>
           </div>
+
           <Text
             className="absolute right-[38%] text-3xl sm:text-[26px] md:text-[28px] text-white-A700 top-[27%]"
             size="txtInterExtraBold30"
@@ -116,8 +191,12 @@ const CriaadeusurioPage = () => {
           >
             Resultado
           </Text>
-          <div className="bg-gray-100 border border-black-900 border-solid h-[301px] rounded-[10px] w-full"></div>
-        </div>
+          <div className="bg-gray-100 border border-black-900 border-solid h-[301px] rounded-[10px] w-full">
+          {/* Exibe o resultado aqui */}
+          {result && (
+            <pre>{JSON.stringify(result, null, 2)}</pre>
+          )}
+        </div>      </div>
       </div>
     </>
   );
--- src/service/api.js
+++ src/service/api.js
@@ -1,8 +1,34 @@
-import { apis } from "service";
+// service/api.js
+import Axios from "axios";
 
-const COMMON_URL = `https://d595-2804-29b8-500b-13b-147e-6b7e-44cf-5769.ngrok-free.app/api/pessoas/`;
+const defaultAxios = Axios.create({
+  headers: { "Content-type": "application/json" },
+});
+defaultAxios.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
 
-const API_URLS = { POST_PESSOAS_API: `${COMMON_URL}` };
+export function apiClient(method, url, options = {}) {
+  const { data = {}, headers = {}, params = {}, ...rest } = options;
+  return defaultAxios({
+    url,
+    data,
+    method,
+    params,
+    headers,
+    ...rest,
+  });
+}
 
-export const postPessoasApi = (payload) =>
-  apis.post(API_URLS.POST_PESSOAS_API, payload);
+export const apis = {
+  get: (url, args) => apiClient("get", url, args),
+  post: (url, args) => apiClient("post", url, args),
+  put: (url, args) => apiClient("put", url, args),
+  patch: (url, args) => apiClient("patch", url, args),
+  delete: (url, args) => apiClient("delete", url, args),
+};
